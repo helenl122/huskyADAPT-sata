bd3ca7ec042f9cce38492e4f96113b2ea5f9ab4a
helenl122 <helenl122@outlook.com>
2025-04-17 18:21:21 -0700

    Fix press entire header to exit; contained press to only red X button

--- components/GameHeader.tsx
+++ components/GameHeader.tsx
@@ -5,7 +5,8 @@ import { useRouter } from 'expo-router';
 const GameHeader = () => {
     const router = useRouter();
     return (
-        <View className="bg-white">
+        <View className="bg-white flex-row">
+            <View className="flex-1"></View>
             <Pressable className="m-4 flex-row justify-end" onPress={() => router.replace('/')}>
                 <MaterialCommunityIcons name={"close-circle"} size={40} color={"red"}/>
             </Pressable>
