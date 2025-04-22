d4773b9d61947a8e4c2599b73bda26da92399f75
helenl122 <helenl122@outlook.com>
2025-04-22 11:29:26 -0700

    Fix press opacity issue

--- components/game_components/DownUpPress.jsx
+++ components/game_components/DownUpPress.jsx
@@ -11,8 +11,7 @@ const DownUpPress = ({label, gameContent}) => {
         accessibilityLabel={label} // what screen reader/switch user hears
         onPressIn={handlePressIn}
         onPressOut={handlePressOut}
-        activeOpacity={0} // no opacity when pressed
-        style={{zIndex:100, flex:1, justifyContent:"center", opacity: 0}}
+        style={{zIndex:100, flex:1, justifyContent:"center", backgroundColor: "transparent"}}
       >
       </TouchableOpacity>
       <View className="absolute left-0 right-0 top-0 bottom-0">{gameContent}</View>
--- components/game_components/SinglePress.jsx
+++ components/game_components/SinglePress.jsx
@@ -10,8 +10,7 @@ const SinglePress = ({label, gameContent}) => {
         accessible={true} // marks element as accessible
         accessibilityLabel={label} // what screen reader/switch user hears
         onPressIn={handlePress}
-        activeOpacity={0} // no opacity when pressed
-        style={{zIndex:100, flex:1, justifyContent:"center", opacity: 0}}
+        style={{zIndex:100, flex:1, justifyContent:"center", backgroundColor:"transparent"}}
       >
       </TouchableOpacity>
       <View className="absolute left-0 right-0 top-0 bottom-0">{gameContent}</View>
