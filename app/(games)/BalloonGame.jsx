6334461ad4005fbddc264180916fe14f5f808d35
Ella Cao <ellacao@Ellas-MacBook-Air-6.local>
2025-04-24 17:48:53 -0700

    fixed png issue, needs tracker to know if a balloon is popped

--- app/(games)/BalloonGame.jsx
+++ app/(games)/BalloonGame.jsx
@@ -55,7 +55,12 @@ const BalloonGame = () => {
             ) : (
               <Image
                 source={require('./Assets/balloon.png')}
-                style={{ width: 100, height: 100, borderRadius: 12 }}
+                style={{
+                  width: 100,
+                  height: 150, // Adjust the height based on the image's aspect ratio
+                  borderRadius: 12,
+                  resizeMode: 'contain' // Ensure the balloon doesn't get cropped
+                }}
               />
             )}
           </Pressable>
